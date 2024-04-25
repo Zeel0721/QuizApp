fc3881d1e42b58abb638e2fd22148e542dc92a84
Zeel0721 <zeelpatel0721@gmail.com>
2024-04-25 19:05:09 +0530

    Created custom validation message for Add Question

--- Nest/src/admin/dto/question.dto.ts
+++ Nest/src/admin/dto/question.dto.ts
@@ -1,33 +1,33 @@
-import { IsBoolean, IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { IsBoolean, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
-export class QuestionDto{
-    @IsNotEmpty()
-    @IsString()
-    question: string;
+export class QuestionDto {
+  @IsNotEmpty({ message: 'Question cannot be empty' })
+  @IsString({ message: 'Question must be String' })
+  question: string;
 
-    @IsNotEmpty()
-    @IsString()
-    optionOne: string;
+  @IsNotEmpty({ message: 'Option A cannot be empty' })
+  @IsString({ message: 'Option A must be String' })
+  optionOne: string;
 
-    @IsNotEmpty()
-    @IsString()
-    optionTwo: string;
+  @IsNotEmpty({ message: 'Option B cannot be empty' })
+  @IsString({ message: 'Option B must be String' })
+  optionTwo: string;
 
-    @IsNotEmpty()
-    @IsString()
-    optionThree?: string;
+  @IsNotEmpty({ message: 'Option C cannot be empty' })
+  @IsString({ message: 'Option C must be String' })
+  optionThree?: string;
 
-    @IsNotEmpty()
-    @IsString()
-    optionFour?: string;
+  @IsNotEmpty({ message: 'Option D cannot be empty' })
+  @IsString({ message: 'Option D must be String' })
+  optionFour?: string;
 
-    @IsNotEmpty()
-    @IsNumber()
-    answer: string;
+  @IsNotEmpty({ message: 'Answer cannot be empty' })
+  @IsNumber()
+  answer: string;
 
-    @IsBoolean()
-    isRequired?: boolean;
+  @IsBoolean()
+  isRequired?: boolean;
 
-    @IsBoolean()
-    isDisabled?: boolean;
+  @IsBoolean()
+  isDisabled?: boolean;
 }
